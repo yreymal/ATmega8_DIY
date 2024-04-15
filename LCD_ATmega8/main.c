4fd259c30c5b0256c8eb75e59f0d120f37b0b3a1
Roman_Yarmak <yreymal@gmail.com>
2024-04-15 19:43:06 +0200

    init

---
+++ LCD_ATmega8/main.c
@@ -0,0 +1,18 @@
+/*
+ * LCD_ATmega8.c
+ *
+ * Created: 4/15/2024 7:35:43 PM
+ * Author : Roman Yarmak
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
